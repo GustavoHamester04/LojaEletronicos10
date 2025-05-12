e9d362fd8e3d326eb8c53d4afc3506648a74980d
Gustavo H <gutahamester@gmail.com>
2025-05-12 19:42:39 -0300

    tela home

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,9 +12,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string>
      */
     protected $middleware = [
-        
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class, // ✅ Correção aqui
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
